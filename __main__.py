5a238c4a4f66fcd8e2d6011e60ef45afd7f1b9e1
juliagarciamaestre <juliagarciamaestre@gmail.com>
2022-03-12 00:52:28 -0300

    eliminate food class

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
+from http.client import CannotSendRequest
 import constants
 
 from game.casting.cast import Cast
-from game.casting.food import Food
 from game.casting.score import Score
 from game.casting.snake import Snake
 from game.scripting.script import Script
@@ -20,10 +20,9 @@ def main():
     
     # create the cast
     cast = Cast()
-    cast.add_actor("foods", Food())
-    cast.add_actor("snakes", Snake())
+    cast.add_actor('snakes',Snake())
     cast.add_actor("scores", Score())
-   
+    cast.add_actor('snake2',Snake())
     # start the game
     keyboard_service = KeyboardService()
     video_service = VideoService()
